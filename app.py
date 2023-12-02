ff82f82af60b54954a0a10cdb5d76c2c20f0678f
eminedari <dari18@itu.edu.tr>
2023-12-02 20:12:25 +0100

    add app.py for deployment

---
+++ app.py
@@ -0,0 +1,53 @@
+# get predictions from the trained model
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the trained model
+model = joblib.load("model.pkl")
+
+##### To use in case where only 2 features are given #####
+# Create a list for all the types
+types = np.arange(0, 3)
+# Create a list for all the categories of accidents
+categories = np.arange(0, 3)
+
+@app.route("/")
+def home():
+    return "Welcome to the accident prediction API!"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        # Get input data from the request
+        data = request.get_json()
+
+        # Extract features
+        year = data["year"]
+        month = data["month"]
+        
+        # check if data only has 2 features
+        if len(data) == 2:
+            # Make a prediction by summing up all the predictions for each type and category
+            prediction = 0
+            for category in categories:
+                for _type in types:
+                    prediction +=int(np.round( model.predict(np.array([[category, _type, year, month]]))[0]))
+            
+        elif len(data) == 4:
+            #gather additional features
+            category = data["category"]
+            _type = data["_type"]
+            # Make a prediction
+            prediction = int(np.round(model.predict(np.array([[category,_type,year,month]]))[0]))
+        
+        # Return the result in the specified format
+        return jsonify({"prediction": prediction})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
